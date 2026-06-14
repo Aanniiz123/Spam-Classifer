1828f0fa33d0f677adc7a5f7d11e89d8a3afe7ed
Aanniiz123 <anishpudasaini25@gmail.com>
2026-06-14 08:44:14 +0545

    adding the nltk

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,8 @@ import nltk
 from app.app import app
 
 nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('punkt_tab')
 sys.path.insert(0, os.path.dirname(__file__))
 
 if __name__ == '__main__':
